f9b2c0b814fb66cf24aaf0228e476a2e41c14861
redlab <daniel_amico@yahoo.com.ar>
2025-04-10 11:50:48 -0300

    prueba de pagina inicial redlabsantafe

---
+++ plantilla.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<title> titulo</title>
+	  <meta charset="utf-8">
+	  	
+	
+</head>
+<body>
+
+<h1>plantilla</h1>
+</body>
+</html>
+
+
---
+++ redlab.blade.php
@@ -0,0 +1,14 @@
+@extends('plantilla')
+
+@section('title', 'redlab')
+
+@section('content')
+
+	<h1>redlab</h1>
+	<p>esta es una pagina nueva</p>
+
+	@include('footer')
+
+@endsection
+
+
